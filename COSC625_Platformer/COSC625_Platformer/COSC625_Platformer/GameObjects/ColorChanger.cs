bd55353804023b2842922d9684e6086684b5bcfb
Zach McNemar <zamcnemar@hotmail.com>
2013-11-12 10:18:10 -0500

    Color Changer Class Tutorial
    I found a cool color iteration class that we can potentially use with
    anything. I wanted to test it out so I used it in the gem class for some
    cool results.

---
+++ COSC625_Platformer/COSC625_Platformer/COSC625_Platformer/GameObjects/ColorChanger.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace COSC625_Platformer.GameObjects
+{
+    public sealed class ColorChanger
+    {
+        private readonly Color[] colors;
+        private float elapsed;
+        private int nextColorIndex = 1;
+        private readonly float timeBetweenColors;
+
+        /// <summary>
+        /// Gets the current color of the object
+        /// </summary
+        /// 
+        public Color CurrentColor
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Get or Set whether or not the ColorChanger is active
+        /// </summary
+        /// 
+        public bool IsActive { get; set; }
+ 
+        public ColorChanger(float timeBetweenColors, bool isActive, params Color[] colors)
+        {
+            this.colors = colors;
+            this.timeBetweenColors = timeBetweenColors;
+            this.IsActive = isActive;
+ 
+            Reset(IsActive);
+        }
+ 
+        public void Reset(bool isActive)
+        {
+            //Set our initial color. If no colors OR we're not active
+            //we set the CurrentColor to White. Otherwise we'll use
+            //the first color in our list of 'colors'
+            if (!IsActive || colors.Length <= 0)
+                CurrentColor = Color.White;
+            else
+                CurrentColor = colors[0];
+ 
+            nextColorIndex = 1;
+            elapsed = 0f;
+            IsActive = isActive;
+        }
+ 
+        public void Update(GameTime gameTime)
+        {
+            //If we deactivate the ColorChanger or we only
+            //use a single color then we don't need to update
+            if (!IsActive || colors.Length == 1)
+                return;
+ 
+            //Keep track of the time so we know when to change colors
+            elapsed += (float)gameTime.ElapsedGameTime.TotalSeconds;
+ 
+            //If it's time to change colors, do so!
+            if (elapsed >= timeBetweenColors)
+            {
+                //If we've passed the last color, start at the beginning
+                if (++nextColorIndex >= colors.Length)
+                    nextColorIndex = 0;
+ 
+                //Reset the time so we can start over again
+                elapsed = 0f;
+            }
+ 
+            //Interpolate our CurrentColor through our list of colors!
+            CurrentColor = Color.Lerp(
+                CurrentColor,
+                colors[nextColorIndex],
+                elapsed / timeBetweenColors);
+        }
+    }
+
+}
--- COSC625_Platformer/COSC625_Platformer/COSC625_Platformer/GameObjects/Items/Star.cs
+++ COSC625_Platformer/COSC625_Platformer/COSC625_Platformer/GameObjects/Items/Star.cs
@@ -14,6 +14,7 @@ namespace COSC625_Platformer.GameObjects.Items
 {
     class Star : Item
     {
+        ColorChanger colorChanger;
 
         public Level Level
         {
@@ -27,7 +28,13 @@ namespace COSC625_Platformer.GameObjects.Items
             this.basePosition = position;
             active = true;
             PointValue = 100;
-            Color = Color.Red;
+
+            colorChanger = new ColorChanger(
+            .25f, // Time between color changes
+            true, // Is the color changer active?
+            Color.Orange, Color.Yellow, Color.Green, // all the colors you want to cycle through
+                Color.Red, Color.Blue, Color.Indigo, Color.Violet);
+            Color = colorChanger.CurrentColor;
 
             LoadContent();
         }
@@ -41,7 +48,9 @@ namespace COSC625_Platformer.GameObjects.Items
         
         public override void Update(GameTime gameTime)
         {
+            colorChanger.Update(gameTime);
             base.Update(gameTime);
+            
         }
          
 
@@ -51,13 +60,18 @@ namespace COSC625_Platformer.GameObjects.Items
             origin = new Vector2(spriteTexture.Width / 2.0f, spriteTexture.Height / 2.0f);
             collectedSound = Level.Content.Load<SoundEffect>("Sounds/Powerup");
             Size = new Rectangle(0, 0, (int)(spriteTexture.Width * Scale), (int)(spriteTexture.Height * Scale));
+
+            // how to use the color changer.
+
         }
 
 
         public override void Draw(SpriteBatch spritebatch, Color color)
         {
 
-            base.Draw(spritebatch, color);
+            Boundary = new Rectangle((int)this.Position.X, (int)this.Position.Y, this.Size.Width, this.Size.Height);
+            spritebatch.Draw(spriteTexture, Position, null, colorChanger.CurrentColor, 0.0f, origin, 1.0f, SpriteEffects.None, 0.0f);
+            //base.Draw(spritebatch, colorChanger.CurrentColor);
         
         }
 
